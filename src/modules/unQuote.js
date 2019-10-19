a388710d947596428847749bc396b1411d70205d
iilei • Jochen Preusche <iilei@users.noreply.github.com>
2019-10-19 11:32:50 +0200

    Minor cleanup

--- src/modules/unQuote.js
+++ src/modules/unQuote.js
@@ -1,4 +1,6 @@
-const quotationMarks = [/^"|'/, /"|'$/]
+// only double quotes - see https://github.com/kba/hocr-spec/blob/master/1.2/spec.md - delimited-string
+
+const quotationMarks = [/^"/, /"$/]
 
 const unQuote = val => val.replace(quotationMarks[0], '').replace(quotationMarks[1], '')
 
