54e089d6379ea46676661d87ca4de69cc15f9bf5
iilei • Jochen Preusche <iilei@users.noreply.github.com>
2019-10-19 15:55:42 +0200

    minor cleanup

--- src/views/tesseract/index.js
+++ src/views/tesseract/index.js
@@ -1,8 +1,5 @@
 import v4_1_0__rc1 from './4.1.0-rc1'
 
-// Property names can be any semverrange string, as seen in
-// https://www.npmjs.com/package/semver
-// so that '1.2.3', '1.x || >=2.5.0 || 5.0.0 - 7.2.3' does satisfy for example '1.2.3'
 export default {
   '4.1.0-rc1': v4_1_0__rc1,
 }
